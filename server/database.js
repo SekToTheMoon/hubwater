e76d0933ba7e7e20d242ef9a17bfc255ea2aa4de
oakkharaphong <6511850002@mut.ac.th>
2024-10-09 19:28:03 +0700

    end_local_main

--- server/database.js
+++ server/database.js
@@ -4,28 +4,25 @@ const mysql = require("mysql2");
 
 const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 // ตั้งค่าการเชื่อมต่อฐานข้อมูล MySQL
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "hubwater",
-//   timezone: "Z",
-// });
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "hubwater",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-const db = mysql.createConnection(
-  `mysql://root:PnsPXYlqnxHnjEBlJBbpLbrsVGlXvLgO@autorack.proxy.rlwy.net:16215/hubwater`
-);
-// const db = mysql.createConnection(urlDB)
-
-// const pool = mysql.createPool(process.env.DB_DATABASEPUBLICE);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hubwater",
+  timezone: "Z",
+});
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hubwater",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+// const db = mysql.createConnection(
+//   `mysql://root:PnsPXYlqnxHnjEBlJBbpLbrsVGlXvLgO@autorack.proxy.rlwy.net:16215/hubwater`
+// );
 
 // เชื่อมต่อกับฐานข้อมูล
 db.connect((err) => {
@@ -36,5 +33,5 @@ db.connect((err) => {
   console.log("Connected to the database");
 });
 
-// module.exports = { db, pool };
-module.exports = { db };
+module.exports = { db, pool };
+// module.exports = { db };
