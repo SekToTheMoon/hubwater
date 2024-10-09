4e9e838a9a7a577ff8075e94b69ace911789b70d
oakkharaphong <6511850002@mut.ac.th>
2024-10-10 03:19:36 +0700

    credential

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const updateStatus = require("./utils/updateStatus");
 const corsOptions = {
   origin: ["http://localhost:5173", "https://hubwater.netlify.app"], // เปลี่ยนเป็นพอร์ตที่ React ใช้ทำงาน
   methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
 // ตั้งค่ามิดเดิลแวร์;
 app.use(express.json());
